802fa6f927004f401bd5834d70600c26c43ad804
Shashikumar <shashirgowdas67@gmail.com>
2022-03-11 14:09:47 +0530

    [Shashi]:code to validate firstname using exception handling

---
+++ Patterns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationException
+{
+    public class Patterns
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,}";
+        public bool ValidateFirstName(string firstName)
+        {
+            if (firstName == null)
+            {
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.Invalid_MESSAGE, "Invalid FirstName");
+            }
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UserRegistrationException;
+
+namespace Day21_UserRegistration_ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program with EXception Handling ");
+            Patterns patternsMatch = new Patterns();
+            Console.WriteLine("Enter FirstName:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("FirstName:" + patternsMatch.ValidateFirstName(firstName));
+        }
+    }
+}
